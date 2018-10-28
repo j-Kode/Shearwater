66a565dab35b52ed042321af217c6693e1038478
Unknown <sidhujeffery@gmail.com>
2018-10-28 14:47:47 -0700

    Shearwater Manifest
    Adding Shearwater solution for decoding manifests

---
+++ ShearwaterTest/DiveManifest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShearwaterTest
+{
+    public class DiveManifest
+    {
+        private Dictionary<int, string> HexByteBlocks { get; }
+        public string ManifestUnits { get; }
+        public bool ValidManifest { get; }
+
+        public DiveManifest(string hexBytes, int unitsBlockOffset)
+        {
+            HexByteBlocks = GetHexByteBlocks(hexBytes);
+            ManifestUnits = GetManifestUnits(unitsBlockOffset);
+            ValidManifest = IsValidManifest();
+        }
+
+        private bool IsValidManifest()
+        {
+            //Checking for valid manifest. Anything else is invalid
+            return HexByteBlocks.Count == 32;
+        }
+
+        private Dictionary<int, string> GetHexByteBlocks(string hexBytes)
+        {
+            //Splitting string into Dictionary for easy access, could have also used array returned but I like dictionaries
+            var hexByteDict = new Dictionary<int, string>();
+            var blockOffset = 0;
+            foreach (var block in hexBytes.Split('-'))
+            {
+                hexByteDict.Add(blockOffset++, block);
+            }
+
+            return hexByteDict;
+        }
+
+        private string GetManifestUnits(int blockOffset)
+        {
+            //Getting the units for the manifest, Units in example maybe wrong as well
+            //UnitSystem: 1, Depth units (0 = meters, 1=feet) of this manifest record and its corresponding log.
+            return Convert.ToInt32(HexByteBlocks[blockOffset], 16) == 0 ? "m" : "ft";
+        }
+
+        public void GetDiveDetails()
+        {
+            if (ValidManifest)
+            {
+                //Getting blocks for data to be displayed and converting them.
+                var diveNoHex = string.Join("", HexByteBlocks.Where(x => Enumerable.Range(2, 2).Contains(x.Key)).Select(x => x.Value));
+                var maxDepthHex = string.Join("", HexByteBlocks.Where(x => Enumerable.Range(16, 2).Contains(x.Key)).Select(x => x.Value));
+                var startTimeHexEpoch = string.Join("", HexByteBlocks.Where(x => Enumerable.Range(4, 4).Contains(x.Key)).Select(x => x.Value));
+
+                //Converting to Epoch, the DiveTime in the example maybe incorrect.
+                //1528452447 = Friday, June 8, 2018 10:07:27 AM
+                var startTimeStamp = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddSeconds(Convert.ToInt32(startTimeHexEpoch, 16));
+
+                //String interpolation not showing correctly here but this would work in c# 6 or later
+                Console.WriteLine($"Dive Number '{Convert.ToInt32(diveNoHex, 16)}', Max Depth: '{(double)Convert.ToInt32(maxDepthHex, 16) / 10}{ManifestUnits}', DiveTime: '{startTimeStamp:yyyy/MM/dd h:mm:ss tt}'");
+            }
+        }
+    }
+}
---
+++ ShearwaterTest/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ShearwaterTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dive1 = new DiveManifest("A5-C4-00-0C-5B-59-98-67-5B-59-98-84-00-00-00-1D-00-86-00-55-00-06-61-00-00-06-63-A0-01-03-06-01", 28);
+            dive1.GetDiveDetails();
+            var dive2 = new DiveManifest("A5-C4-00-0B-5B-1C-E8-B8-5B-1C-FF-91-00-00-16-D9-07-2B-02-E5-00-05-60-20-00-05-AD-60-01-03-00-01", 28);
+            dive2.GetDiveDetails(); 
+            var dive3 = new DiveManifest("5A-23-00-0A-5B-1B-96-DD-5B-1B-AC-A1-00-00-15-C4-05-EC-02-DB-00-05-0E-80-00-05-56-80-01-03-00-01", 28);
+            dive3.GetDiveDetails();
+            var dive4 = new DiveManifest("A5-C4-00-09-5B-1A-55-5F-5B-1A-6A-A0-00-00-15-41-07-C0-02-F6-00-04-B8-80-00-05-00-80-01-03-00-01", 28);
+            dive4.GetDiveDetails();
+        }
+    }
+}
